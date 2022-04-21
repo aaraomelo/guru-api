cf65e8d1a6a6bf496e44785e5e9e1a3f6605f8d9
Aarão Melo <aarao.melo@shiftinc.com.br>
2022-04-20 21:25:17 -0400

    create user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,26 +10,42 @@ import {
 import { UserService } from './user.service';
 import { User as UserModel } from '@prisma/client';
 
-@Controller()
+@Controller('user')
 export class UserController {
   constructor(
     private readonly userService: UserService,
   ) { }
 
-  @Get('user')
+  @Get()
   async getPublishedPosts(): Promise<UserModel[]> {
     return this.userService.users({
       where: {},
     });
   }
 
-  @Post('user')
+  @Get(':id')
+  async getFilteredPosts(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.user({ id: Number(id) });
+  }
+
+  @Post()
   async signupUser(
     @Body() userData: { name: string; email: string, password: string },
   ): Promise<UserModel> {
     return this.userService.createUser(userData);
   }
 
+  @Put(':id')
+  async updateUser(@Param('id') id: string, @Body() userData: UserModel): Promise<UserModel> {
+    return this.userService.updateUser({
+      where: { id: Number(id) },
+      data: { ...userData },
+    });
+  }
 
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.deleteUser({ id: Number(id) });
+  }
 
 }
